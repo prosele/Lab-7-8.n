e8eea9149b7fb7d27880e4ff1f309fd8ae1a5b5d
Kseniia Prokopovich <prokopovichksenia@gmail.com>
2023-12-13 19:37:33 +0300

    Initial Commit

---
+++ Lab 7-8.n/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab 7-8.n
+//
+//  Created by Ксения Прокопович on 13.12.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
